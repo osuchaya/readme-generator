dba2cb6e45c9762f276fbf7ed34f8fd5ff3ab7de
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-04-18 18:11:57 -0700

    Import all packages and dependencies

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const createMD = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
